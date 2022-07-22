b1c3e45d2f296463ea4fa1cdeaf99b0468e9ebc2
Duc <ngoduc2277@gmail.com>
2022-07-22 15:04:22 +0700

    first commit

--- setup.py
+++ setup.py
@@ -3,29 +3,34 @@ import sys
 from distutils.core import setup
 
 import py2exe
+from py2exe.build_exe import isSystemDLL
 
 origIsSystemDLL = py2exe.build_exe.isSystemDLL
+
+
 def isSystemDLL(pathname):
     dlls = ("libfreetype-6.dll", "libogg-0.dll", "sdl_ttf.dll")
     if os.path.basename(pathname).lower() in dlls:
         return 0
     return origIsSystemDLL(pathname)
+
+
 py2exe.build_exe.isSystemDLL = isSystemDLL
 
 sys.argv.append('py2exe')
 
 setup(
-    name =    'Flappy Bird',
-    version = '1.0',
-    author =  'Sourabh Verma',
-    options = {
+    name='Flappy Bird',
+    version='1.0',
+    author='Sourabh Verma',
+    options={
         'py2exe': {
-            'bundle_files': 1, # doesn't work on win64
+            'bundle_files': 1,  # doesn't work on win64
             'compressed': True,
         }
     },
 
-    windows = [{
+    windows=[{
         'script': "flappy.py",
         'icon_resources': [
             (1, 'flappy.ico')
